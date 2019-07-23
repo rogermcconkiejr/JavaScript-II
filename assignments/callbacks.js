6bd0107a5909f04393b521301be5fa867f4f94da
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-07-23 14:25:48 -0600

    finished with array problems

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,7 +2,7 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+
 
   //Given this problem: 
   
@@ -22,20 +22,36 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     console.log(first)
   });
 
-*/
+
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) { 
+console.log(length);
+})
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, function(lasti) {
+  console.log(lasti);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return x + y === cb;
 }
+
+sumNums(2, 3, function(added){
+  console.log(added);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
